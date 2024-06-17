269d52b029bd65311af9569398625acd9eed1ccd
suresh5189 <suru242000@gmail.com>
2024-06-17 21:23:02 +0530

    File Updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,118 @@
 import express from "express";
-import { loginController, registerController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
+import ratelimit from "express-rate-limit";
 
-//router object
+// IP Limiter
+const limiter = ratelimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+//Router Object
 const router = express.Router();
 
-// routes
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - name
+ *        - lastName
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: The Auto-generated id of user collection
+ *        name:
+ *          type: string
+ *          description: User Name
+ *        lastName:
+ *          type: string
+ *          description: User Last Name
+ *        email:
+ *          type: string
+ *          description: User Email Address
+ *        password:
+ *          type: string
+ *          description: User Password Should Be Greater Than 6 Character
+ *        location:
+ *          type: string
+ *          description: User Location City Or Country
+ *      example:
+ *        id: SFNIENFSENFNK343
+ *        name: John
+ *        lastName: Doe
+ *        email: johndoe@gmail.com
+ *        password: test@12345
+ *        location: mumbai
+ */
+
+/**
+ *  @swagger
+ *  tags:
+ *    name: Auth
+ *    description: authentication apis
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *    post:
+ *      summary: Register New User
+ *      tags: [Auth]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *      responses:
+ *        200:
+ *          description: User Created Successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        500:
+ *          description: Internal Server Error
+ */
+
+// Routes
 // Register || POST
-router.post("/register", registerController);
+router.post("/register", limiter, registerController);
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *  post:
+ *    summary: Login Page
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: Login Successfull
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      500:
+ *        description: Something Went Wrong
+ */
 
 // Login || Post
-router.post("/login",loginController);
-
+router.post("/login", limiter, loginController);
 
 export default router;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+// API Documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
 // imports packages
 import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+
+// Security Packages
+import helmet from "helmet";
+import xss from "xss-clean";
+import ExpressMongoSanitize from "express-mongo-sanitize";
 
 // file imports
 import connectDB from "./config/db.js";
@@ -21,10 +30,32 @@ dotenv.config();
 // mongoDB connection
 connectDB();
 
+// Swagger API Config
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Job Portal Application",
+      description: "Node Expresjs Job Portal Application",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spec = swaggerJSDoc(options);
+
 // Rest Object
 const app = express(); // All features of express are in app variable
 
 // middlewares
+app.use(helmet());
+app.use(xss());
+app.use(ExpressMongoSanitize());
 // The express. json() function is a middleware function used in Express. js applications to parse incoming JSON data from HTTP requests, a standard format for data transmission in web servers.
 app.use(express.json());
 app.use(cors());
@@ -35,6 +66,9 @@ app.use("/api/v1/test", testRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/job", jobsRoutes);
+
+// Homeroute Root
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 
 // validation middleware
 app.use(errorMiddleware);
